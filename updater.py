551734c4312f4a431e62d15f3a10ef14ddc84737
shivaru <pvs34@ya.ru>
2020-05-07 15:18:01 +0400

    test

---
+++ updater.py
@@ -0,0 +1,41 @@
+import requests
+import sys  # sys нужен для передачи argv в QApplication
+from PyQt5 import QtWidgets
+import update  # Это наш конвертированный файл дизайна
+import os
+
+
+def update():
+        urls = ['https://raw.github.com/Shivaru/waybill/master/README.md',
+                'https://raw.github.com/Shivaru/waybill/master/design.py',
+                'https://raw.github.com/Shivaru/waybill/master/Excel.py',
+                'https://raw.github.com/Shivaru/waybill/master/GetDataWialon.py',
+                'https://raw.github.com/Shivaru/waybill/master/Window.py',
+                'https://raw.github.com/Shivaru/waybill/master/GA4-C.xls',
+                'https://raw.github.com/Shivaru/waybill/master/LA.xls',
+                'https://raw.github.com/Shivaru/waybill/master/updater.py',
+                'https://raw.github.com/Shivaru/waybill/master/main.py',
+                'https://raw.github.com/Shivaru/waybill/master/version.txt']
+
+        r = requests.get(urls[9])
+        versgit = r.text
+
+        path = os.getcwd()
+        verspath = path + "\\version.txt"
+        versfile = open(verspath,'r')
+        version = (versfile.read())
+
+        print(versgit, version)
+
+        if versgit == version:
+                print("Versii sovpadaut")
+                return "Обновлений нет"
+        else:
+                print("Versii nesovpadaut")
+                for link in urls:
+                        lcnt = requests.get(link)
+                        name = link.split("/")
+                        print(name[-1])
+                        # with open(name, "wb") as code:
+                        #      code.write(r.content)
+                return "Программа обновлена"
